9e7d6267e1df0e1e4e8f7de164242e3751405e5f
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2011-07-22 11:21:07 +0000

    Clean up
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@29 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- src/com/spshop/admin/client/QueryServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.spshop.admin.client;
-
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.spshop.model.QueryCriteria;
-import com.spshop.model.Component;
-public interface QueryServiceAsync {
-	void greetServer(QueryCriteria criteria, AsyncCallback<QueryResult<Component>> callback) throws IllegalArgumentException;
-}
