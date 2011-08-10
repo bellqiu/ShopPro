4fe6c587d756ed2c80b572d55a701911470bbabe
realliam@gmail.com <realliam@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2011-08-10 04:12:43 +0000

    Refined the architecture of action to make super action populate common data
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@96 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- src/com/spshop/fe/actions/BaseAction.java
+++ src/com/spshop/fe/actions/BaseAction.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 
 import com.spshop.cache.CacheMap;
 import com.spshop.fe.formbeans.PageFormBean;
@@ -14,31 +18,31 @@ import com.spshop.model.Product;
 import com.spshop.model.Site;
 import com.spshop.utils.AllConstants;
 
-public class BaseAction extends Action {
-	public void populateMenuBar(PageFormBean page) {
+public abstract class BaseAction extends Action {
+	
+	private PageFormBean page;
+	private String[] uris;
+	
+	
+	/**
+	 * Populate MenuBar data for page
+	 * 
+	 * @param page
+	 */
+	private void populateMenuBar(PageFormBean page) {
 		List<Category> categories = new ArrayList<Category>();
 		categories = (List<Category>) CacheMap.getInstance().getCache(AllConstants.CATEGORY_CACHE);
 		
 		page.addAllCategories(categories);
 	}
-	
-	public void populateProductsByCategory(PageFormBean page) {
-		List<Product> products = new ArrayList<Product>();
-		//products = ServiceFactory.getService(ProductService.class).findPageByPage(0, 24);
-		
-		//Mockup data
-		for (int i = 0; i < 24; i++) {
-			Product product = new Product();
-			product.setName("Product - " + i);
-			product.setTitle("Pro - Title - " + i);
-			products.add(product);
-			//product.setImages(images);
-		}
-		
-		page.setCatProducts(products);
-	}
-	
-	public void populateSiteInfo(HttpServletRequest request, PageFormBean page) {
+
+	/**
+	 * Populate Site Informations for page
+	 * 
+	 * @param request
+	 * @param page
+	 */
+	private void populateSiteInfo(HttpServletRequest request, PageFormBean page) {
 		//Mockup data
 		Site site = new Site();
 		site.setDomain(request.getServerName() + ":8080");
@@ -51,7 +55,7 @@ public class BaseAction extends Action {
 		page.setCategory(searchCategory(categories, categoryName));
 	}
 	
-	public Category searchCategory(List<Category> categories, String catName) {
+	private Category searchCategory(List<Category> categories, String catName) {
 		Category result = null;
 		
 		for (Category category : categories) {
@@ -66,4 +70,41 @@ public class BaseAction extends Action {
 		
 		return result;
 	}
+	
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		setPage((PageFormBean) form);
+		uris = request.getRequestURI().split(AllConstants.URL_SEPERATOR);
+		populateMenuBar(getPage());
+		populateSiteInfo(request, getPage());
+		
+		return processer(mapping, form, request, response);
+	}
+	
+	/**
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public abstract ActionForward processer(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+	public void setPage(PageFormBean page) {
+		this.page = page;
+	}
+
+	public PageFormBean getPage() {
+		return page;
+	}
+	
+	public String[] getUris() {
+		return uris;
+	}
+
+	public void setUris(String[] uris) {
+		this.uris = uris;
+	}
+
 }
--- src/com/spshop/fe/actions/PageAction.java
+++ src/com/spshop/fe/actions/PageAction.java
@@ -1,5 +1,8 @@
 package com.spshop.fe.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,36 +11,41 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.spshop.fe.formbeans.PageFormBean;
+import com.spshop.model.Product;
 import com.spshop.utils.AllConstants;
 
 public class PageAction extends BaseAction {
 	@Override
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		
-		PageFormBean page = (PageFormBean) form;
-		
-		
-		String[] uris = request.getRequestURI().split(AllConstants.URL_SEPERATOR);
-		
-		if (uris.length != 0) {
-			if (AllConstants.CATEGORY_URL.equals(uris[1])) {
+	public ActionForward processer(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (getUris().length != 0) {
+			if (AllConstants.CATEGORY_URL.equals(getUris()[1])) {
 				// TODO Not implemented so far, for category page
-				populateProductsByCategory(page);
-				populateCategoryForCategoryPage(uris[2], page);
-			} else if (AllConstants.KEYWORDS_URL.equals(uris[0])) {
+				populateProductsByCategory(getPage());
+				populateCategoryForCategoryPage(getUris()[2], getPage());
+			} else if (AllConstants.KEYWORDS_URL.equals(getUris()[0])) {
 				// TODO Not implemented so far, for keywords page
 			} else {
 				// TODO Other forward
 			}
 		}
-		
-		populateMenuBar(page);
-		
-		populateSiteInfo(request, page);
-		
+
 		return mapping.findForward(AllConstants.SUCCESS_VALUE);
 	}
 	
+	public void populateProductsByCategory(PageFormBean page) {
+		List<Product> products = new ArrayList<Product>();
+		//products = ServiceFactory.getService(ProductService.class).findPageByPage(0, 24);
+		
+		//Mockup data
+		for (int i = 0; i < 24; i++) {
+			Product product = new Product();
+			product.setName("Product - " + i);
+			product.setTitle("Pro - Title - " + i);
+			products.add(product);
+			//product.setImages(images);
+		}
+		
+		page.setCatProducts(products);
+	}
+
 }
