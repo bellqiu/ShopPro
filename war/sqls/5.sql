e0837bc4d4dbb1b3597ab4aa1510c01444a42536
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2011-09-08 13:52:07 +0000

    update delta sqls
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@191 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- war/sqls/5.sql
+++ war/sqls/5.sql
@@ -1,15 +1,12 @@
--- ----------------------------
--- Table structure for `shop_html`
--- ----------------------------
 DROP TABLE IF EXISTS `shop_html`;
 CREATE TABLE `shop_html` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `createDate` datetime DEFAULT NULL,
   `updateDate` datetime DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
+  `content` text,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 
 alter table SHOP_TabProduct add(createDate date);
