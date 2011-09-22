6f90a65e842fa4e364dbbed63190b95a62640c1f
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2011-09-22 13:44:47 +0000

    Shopping cart and litter bug fix
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@231 c8aa77e7-4d14-485d-9416-cb5e655923dd

---
+++ src/com/spshop/model/cart/ShoppingCart.java
@@ -0,0 +1,85 @@
+package com.spshop.model.cart;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.spshop.model.Order;
+import com.spshop.model.OrderItem;
+import com.spshop.model.Product;
+import com.spshop.model.UserOption;
+
+public class ShoppingCart {
+	private Order order;
+
+	public ShoppingCart(Order order) {
+		this.setOrder(order);
+		if(null==order.getItems()){
+			order.setItems(new ArrayList<OrderItem>());
+		}
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void addItem(Product product, List<UserOption> options, int qty) {
+		if(null!=order.getItems()){
+			boolean existItem = false;
+			for (OrderItem item : order.getItems()) {
+				if(item.getProduct().getName().equals(product.getName())){
+					if(options.containsAll(item.getUserOptions())&&item.getUserOptions().containsAll(options)){
+						existItem = true;
+						item.setQuantity(item.getQuantity()+qty);
+						item.setUpdateDate(new Date());
+						item.setFinalPrice((float)(item.getQuantity()*product.getActualPrice()));
+					}
+				}
+			}
+			
+			if(!existItem){
+				OrderItem orderItem = new OrderItem();
+				orderItem.setName(product.getName()+"-"+System.nanoTime());
+				orderItem.setProduct(product);
+				orderItem.setUserOptions(options);
+				orderItem.setCreateDate(new Date());
+				orderItem.setUpdateDate(new Date());
+				orderItem.setQuantity(qty);
+				orderItem.setFinalPrice((float)(qty*product.getActualPrice()));
+				order.getItems().add(orderItem);
+			}
+			
+		}
+	}
+
+	public void update(String itemName, int qty) {
+		if(null!=order.getItems()){
+			for (OrderItem item : order.getItems()) {
+				if(item.getName().equals(itemName)){
+					item.setQuantity(qty);
+					item.setFinalPrice((float)(item.getProduct().getActualPrice()*qty));
+				}
+			}
+		}
+	}
+
+	public void remove(String itemName) {
+		if(null!=order.getItems()){
+			OrderItem toRemoveItem = null;
+			for (OrderItem item : order.getItems()) {
+				if(item.getName().equals(itemName)){
+					toRemoveItem = item;
+				}
+			}
+			if(null!=toRemoveItem){
+				order.getItems().remove(toRemoveItem);
+			}
+		}
+	}
+	
+	
+}
