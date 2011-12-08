1435792f6893ee7fea1e4b1b020305ec28572c9a
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2011-12-08 14:08:15 +0000

    404 & 301
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@336 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- src/com/spshop/fe/actions/BaseAction.java
+++ src/com/spshop/fe/actions/BaseAction.java
@@ -140,12 +140,12 @@ public abstract class BaseAction extends Action {
 	 }
 	 
 	 private void dealURL( HttpServletRequest request, HttpServletResponse response, String domain) throws IOException{
-		 
+		/* 
 		 String noPrefixDomain = domain.replaceFirst("(?i)(^http://)(www\\.)*", "");
 		 String url = request.getRequestURL().toString();
 		 if(url.matches(noPrefixDomain)){
 			 response.sendRedirect(domain);
-		 }
+		 }*/
 	 }
 	 
 	 public static void main(String[] args) {
