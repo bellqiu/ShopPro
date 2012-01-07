e4b2170740f3ab47e7ed72ddee7bc548d51a5066
realliam@gmail.com <realliam@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2012-01-07 16:14:58 +0000

    Subscribe email page
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@431 c8aa77e7-4d14-485d-9416-cb5e655923dd

---
+++ war/sqls/14.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sshop`.`shop_email` CHANGE COLUMN `createData` `createDate` DATETIME NULL DEFAULT NULL  ;
+ALTER TABLE `sshop`.`shop_email` CHANGE COLUMN `email_address` `emailAddress` VARCHAR(100) NULL DEFAULT NULL  ;
