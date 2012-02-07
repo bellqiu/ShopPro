c7f226d0cef1b7fd7a23d518a09e0d4117863097
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2012-02-07 11:53:49 +0000

    conf file
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@489 c8aa77e7-4d14-485d-9416-cb5e655923dd

---
+++ src/com/spshop/utils/TempleteParser.java
@@ -0,0 +1,16 @@
+package com.spshop.utils;
+
+import java.util.Map;
+
+public class TempleteParser {
+	/**
+	 * 
+	 * @param templeteType
+	 * @param variables
+	 * @return
+	 */
+	public static String parseMailContent(String templeteType, Map<String,Object> variables){
+		
+		return "test";
+	}
+}
