d29cf28c2c2f6ec9e5f9a311e0bf89cf61f92de3
szhu.spark@gmail.com <szhu.spark@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2012-03-08 13:03:51 +0000

    User center mock up
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@585 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- src/com/spshop/web/UserCenterController.java
+++ src/com/spshop/web/UserCenterController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserCenterController extends BaseController{
-	@RequestMapping("/register")
-	public String helloWorld(Model model) {
-		return "register";
+	@RequestMapping("/changePwd")
+	public String changePwd(Model model) {
+		return "changePwd";
+	}
+	
+	@RequestMapping("/userProfile")
+	public String userProfile(Model model) {
+		return "userProfile";
 	}
 }
