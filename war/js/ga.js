0b56742b8f1cb19197f4ed60a8fe526ff69a2b58
kabist@gmail.com <kabist@gmail.com@c8aa77e7-4d14-485d-9416-cb5e655923dd>
2012-03-20 12:52:26 +0000

    add code for Tracking Facebook Ads With Google Analytics
    git-svn-id: https://sp-b2cv1.googlecode.com/svn/trunk@606 c8aa77e7-4d14-485d-9416-cb5e655923dd

--- war/js/ga.js
+++ war/js/ga.js
@@ -1,5 +1,10 @@
 var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-27926805-1']);
+  _gaq.push(['_setCampSourceKey', 'utm_source']);
+  _gaq.push(['_setCampMediumKey', 'utm_medium']);
+  _gaq.push(['_setCampContentKey', 'utm_content']);
+  _gaq.push(['_setCampTermKey', 'utm_keyword']);
+  _gaq.push(['_setCampNameKey', 'utm_campaign']);
   _gaq.push(['_trackPageview']);
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
